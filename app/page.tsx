37bfa2fbb4e16ecc18fc2d02b5a61f5930c3a520
jpoechill <poamrong@rith.com>
2023-11-09 00:00:46 -0800

    add routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between pt-24">
       <div className='bg-white overflow-hidden p-[0px] rounded-[30px] max-w-[930px] h-[640px] max-h-[640px]'>
         <div className="grid grid-cols-2 gap-0 h-full max-h-[640px]">
           <div className='text-[15px] pt-[80px] pl-[60px] pr-[30px]'>
@@ -11,13 +12,11 @@ export default function Home() {
             <p className='my-2'><img src="/icon-list.svg" className="inline mr-3" alt="" />Product discovery and building what matters</p>
             <p className='my-2'><img src="/icon-list.svg" className="inline mr-3" alt="" />Measuring to ensure updates are a success</p>
             <p className='my-2'><img src="/icon-list.svg" className="inline mr-3" alt="" />And much more!</p>
-
-            <br /><br />
-
-            <p className='py-2 font-bold text-xs'>Email address</p>
+            <p className='mt-10 py-2 font-bold text-xs'>Email address</p>
             <input className="border rounded-xl w-full h-[55px] p-5" type="email" name="" id="" placeholder="email@company.com" />
-            <br />
-            <button className="rounded-xl bg-[#242642] text-white font-bold w-full h-[55px] p-3 my-5">Subscribe to monthly newsletter</button>
+            <Link href="/about">
+              <button className="rounded-xl bg-[#242642] text-white font-bold w-full h-[55px] p-3 my-5">Subscribe to monthly newsletter</button>
+            </Link>
           </div>
           <div className="h-full  max-h-[640px] p-[20px]">
             <div className='rounded-[20px] fit-img top-0'>
---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between pt-24">
+      <div className='bg-black text-white'>hello</div>
+    </div >
+  )
+}
+
