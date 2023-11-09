29a0e8f756a015e1772a011f0a5e9dead7a34b8b
jpoechill <poamrong@rith.com>
2023-11-08 22:10:00 -0800

    refine desktop

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className='bg-white overflow-hidden p-[0px] rounded-[30px] max-w-[930px] h-[640px] max-h-[640px]'>
         <div className="grid grid-cols-2 gap-0 h-full max-h-[640px]">
-          <div className='text-[15px] pt-[80px] pl-[65px]'>
-            <p className='text-4xl font-bold pb-[25px]'>Stay updated!</p>
+          <div className='text-[15px] pt-[80px] pl-[60px] pr-[30px]'>
+            <p className='text-[58px] font-bold pb-[25px]'>Stay updated!</p>
             <p className='mb-5'>Join 60,000+ product managers receiving monthly updates on:</p>
             <p className='my-2'><img src="/icon-list.svg" className="inline mr-3" alt="" />Product discovery and building what matters</p>
             <p className='my-2'><img src="/icon-list.svg" className="inline mr-3" alt="" />Measuring to ensure updates are a success</p>
@@ -14,15 +14,10 @@ export default function Home() {
 
             <br /><br />
 
-            <p>Email address</p>
-
-            <br /><br />
-
-            <input type="email" name="" id="" value="email@company.com" />
+            <p className='py-2 font-bold text-xs'>Email address</p>
+            <input className="border rounded-xl w-full h-[55px] p-5" type="email" name="" id="" placeholder="email@company.com" />
             <br />
-            <button>
-              Subscribe to monthly newsletter
-            </button>
+            <button className="rounded-xl bg-[#242642] text-white font-bold w-full h-[55px] p-3 my-5">Subscribe to monthly newsletter</button>
           </div>
           <div className="h-full  max-h-[640px] p-[20px]">
             <div className='rounded-[20px] fit-img top-0'>
