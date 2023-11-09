e8009ca61f86787c8893ad4f747d48fd8d0120f5
jpoechill <poamrong@rith.com>
2023-11-09 00:23:31 -0800

    add gradient button

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             <p className='mt-10 py-2 font-bold text-xs'>Email address</p>
             <input className="border rounded-xl w-full h-[55px] p-5" type="email" name="" id="" placeholder="email@company.com" />
             <Link href="/about">
-              <button className="rounded-xl bg-[#242642] text-white font-bold w-full h-[55px] p-3 my-5">Subscribe to monthly newsletter</button>
+              <button className="rounded-xl bg-[#242642] text-white font-bold w-full h-[55px] p-3 my-5 hover:text-blac hover:bg-gradient-to-r from-[#FA5076] to-[#F86739] hover:shadow-2xl hover:shadow-[#FF5E5C] hover:transition-all">Subscribe to monthly newsletter</button>
             </Link>
           </div>
           <div className="h-full  max-h-[640px] p-[20px]">
