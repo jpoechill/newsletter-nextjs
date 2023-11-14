9e0a1c361d78fd5eca7c5cd33b214b88cae32ab5
jpoechill <poamrong@rith.com>
2023-11-14 10:56:39 -0800

    fix file path

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useForm } from "react-hook-form";
 import Image from 'next/image'
 import Link from 'next/link';
-import Form from '/components/Form'
+import Form from '../components/Form'
 export default function Home() {
 
   const methods = useForm()
