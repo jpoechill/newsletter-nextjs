ba9b4ccac49c85ff3c6ff7a61424786fe6eb69a6
jpoechill <poamrong@rith.com>
2023-11-14 11:14:32 -0800

    fix .tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -26,7 +26,7 @@ export default function Form() {
   };
 
   const validateEmail: boolean = (email: string) => {
-    var re = /\S+@\S+\.\S+/;
+    var re = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     return re.test(email);
   }
 
